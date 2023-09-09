62fc821ffeabbb3be8912ec6b60b78c01171b28e
Joshua Nash <joshuangola@gmail.com>
2023-09-09 01:23:01 -0600

    working

---
+++ src/components/MovieScreen.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieScreen = ({
+  addMovie,
+  movieList,
+  page,
+  setPage,
+  list,
+  removeMovie,
+}) => {
+  const decrement = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const increment = () => setPage(page + 1);
+
+  const movieDisplay = movieList.map((movie) => {
+    return (
+      <MovieCard
+        addMovie={addMovie}
+        movie={movie}
+        list={list}
+        removeMovie={removeMovie}
+      />
+    );
+  });
+
+  return (
+    <div className="page">
+      <h1>Devmountain Movie Theatre</h1>
+      <h3>Add a movie to your watchlist!</h3>
+      <div className="btn-container">
+        <button onClick={decrement}>Previous</button>
+        <button onClick={increment}>Next</button>
+      </div>
+      <div className="movie-container">{movieDisplay}</div>
+    </div>
+  );
+};
+
+export default MovieScreen;
---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,15 @@
+import MovieCard from "./MovieCard";
+
+const Watchlist = ({ list, removeMovie }) => {
+  const movieDisplay = list.map((movie) => {
+    return <MovieCard movie={movie} removeMovie={removeMovie} list={list} />;
+  });
+  return (
+    <div className="watchlist">
+      <h1>My Watchlist</h1>
+      <div className="movie-container">{movieDisplay}</div>
+    </div>
+  );
+};
+
+export default Watchlist;
